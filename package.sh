368f1757c4e33a02d93bf8399e2c12ec5e1783db
Ike Devolder <ike.devolder@studioemma.eu>
2017-01-13 13:52:05 +0100

    package :: make sure the created boxes only have the insecure key
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- package.sh
+++ package.sh
@@ -4,5 +4,9 @@
 name="$1"
 
 vagrant destroy
+sed -e 's/#config.ssh.insert_key/config.ssh.insert_key/' \
+    -i Vagrantfile
 vagrant up
 vagrant package --output "se-$name-$(date +%Y%m%d).box"
+sed -e 's/config.ssh.insert_key/#config.ssh.insert_key/' \
+    -i Vagrantfile
