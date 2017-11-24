993e13729484eae0cf828e7ffa56f369aa5f25b6
Ike Devolder <ike.devolder@studioemma.eu>
2017-11-24 17:32:22 +0100

    because of password auth we must first halt and then up again to get vagrant ssh working while packaging
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- package.sh
+++ package.sh
@@ -7,6 +7,8 @@ vagrant destroy -f
 sed -e 's/#config.ssh.insert_key/config.ssh.insert_key/' \
     -i Vagrantfile
 vagrant up
+vagrant halt
+vagrant up
 vagrant ssh
 vagrant package --output "se-$name-$(date +%Y%m%d).box"
 sed -e 's/config.ssh.insert_key/#config.ssh.insert_key/' \
