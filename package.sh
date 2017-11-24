de28313bf3614fe2b707520f2a3609f2ed2e2b52
Ike Devolder <ike.devolder@studioemma.eu>
2017-11-24 17:51:01 +0100

    when restarting after building for package first change vagrantfile back
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- package.sh
+++ package.sh
@@ -8,8 +8,8 @@ sed -e 's/#config.ssh.insert_key/config.ssh.insert_key/' \
     -i Vagrantfile
 vagrant up
 vagrant halt
+sed -e 's/config.ssh.insert_key/#config.ssh.insert_key/' \
+    -i Vagrantfile
 vagrant up
 vagrant ssh
 vagrant package --output "se-$name-$(date +%Y%m%d).box"
-sed -e 's/config.ssh.insert_key/#config.ssh.insert_key/' \
-    -i Vagrantfile
