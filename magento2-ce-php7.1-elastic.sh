09d06a5226b4a6e440688a3c1b28aeebca460b33
Ike Devolder <ike.devolder@studioemma.eu>
2018-06-20 11:53:15 +0200

    simplify the usage of this repo, make everything available in master for php 7.0 and php 7.1
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

---
+++ magento2-ce-php7.1-elastic.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+basedir=$(dirname $(readlink -f $0))
+calldir=$(pwd)
+
+if [ "/tmp" = "$basedir" ]; then
+    cd /vagrant/manifests
+else
+    cd "$basedir/manifests"
+fi
+
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+# install 'modules'
+init/install.sh
+sshd/install.sh
+bash/install.sh
+nginx/install.sh
+php/install-7.0.sh
+mysql/install.sh
+redis/install.sh
+memcached/install.sh
+nodejs/install.sh
+grunt/install.sh
+mailcatcher/install.sh
+varnish/install.sh
+elasticsearch/install-5.sh # elasticsuite and magento commerce supported
+magento/install.sh
+mountdependency/install.sh -m '/var/www/website' -s 'nginx' -s 'php7.0-fpm'
+
+if [ "/tmp" != "$basedir" ]; then
+    cd "$calldir"
+fi
